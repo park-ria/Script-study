45b5b568785a6126a5bc91f820fbe079c8160758
park-ria <danny6217@gmail.com>
2024-08-16 11:49:59 +0900

    대입연산자 1번 문제

---
+++ 240816(Assignment Operators)/Assignment Operators01/script.js
@@ -0,0 +1,28 @@
+let num1 = 10;
+const num2 = 3;
+/*console.log(`현재 num1:${num1}, num2:${num2}`);
+console.log("Q1) num1=num1+num2을 대입연산자로 풀어주세요.");
+console.log(`A1) num1은 ${(num1 += num2)}입니다.`);
+console.log("");
+console.log(`현재 num1:${num1}, num2:${num2}`);
+console.log("Q2) num1=num1-num2을 대입연산자로 풀어주세요.");
+console.log(`A2) num1은 ${(num1 -= num2)}입니다.`);
+console.log("");
+console.log(`현재 num1:${num1}, num2:${num2}`);
+console.log("Q2) num1=num1*num2을 대입연산자로 풀어주세요.");
+console.log(`A2) num1은 ${(num1 *= num2)}입니다.`);
+console.log("");
+console.log(`현재 num1:${num1}, num2:${num2}`);
+console.log("Q2) num1=num1/num2을 대입연산자로 풀어주세요.");
+console.log(`A2) num1은 ${(num1 /= num2)}입니다.`);
+console.log("");
+console.log(`현재 num1:${num1}, num2:${num2}`);
+console.log("Q2) num1=num1%num2을 대입연산자로 풀어주세요.");
+console.log(`A2) num1은 ${(num1 %= num2)}입니다.`);*/
+
+// 정답
+console.log((num1 += num2));
+console.log((num1 -= num2));
+console.log((num1 *= num2));
+console.log((num1 /= num2));
+console.log((num1 %= num2));
